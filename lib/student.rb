0f7a025a1a88ef8a1bd5181bcd076b40a6f0a789
Anthony Pardo <pardo.anthony@gmail.com>
2020-02-28 15:57:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,6 +55,11 @@ class Student
     student.save
     student
   end
+  
+  def self.new_from_db(row)
+    student = Student.new(row[0], row[1], row[2])
+    student
+  end
  
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
